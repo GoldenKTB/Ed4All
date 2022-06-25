f5111a2418a8c84f3ee82d4669ed5f62acf8a658
GoldenKTB <107734531+GoldenKTB@users.noreply.github.com>
2022-06-25 12:43:06 -0500

    Updated website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 
 ## Basic local website.
 
@@ -6,4 +6,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route("/problem")
+def problem():
+    return render_template("problem.html")
+
+@app.route("/solution")
+def solution():
+    return render_template("Goals of solution.html")
+
+@app.route("/prototype")
+def prototype():
+    return render_template("prototype.html")
+
+if __name__ == "__main__":
+    app.run()
