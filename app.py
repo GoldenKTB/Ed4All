fab1d9b90d38af2ef4c0b1f3a32253373238c773
GoldenKTB <107734531+GoldenKTB@users.noreply.github.com>
2022-06-26 17:53:37 -0500

    Made stuff work

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 from translation import english, spanish, german, mandarin, finnish
 
 app = Flask(__name__)
@@ -16,9 +15,35 @@ def problem():
 def solution():
     return render_template("Goals of solution.html")
 
-@app.route("/prototype")
+@app.route("/prototype", methods=['GET', 'POST'])
 def prototype():
-    return render_template("prototype.html")
+    if request.method == "POST":
+        select_start = request.form.get('start')
+        select_end = request.form.get('end')
+        translate = request.form.get('text_to_translate')
+        match select_start:
+            case "English":
+                result = english(select_end.lower(), translate)
+                print(result)
+                return render_template("prototype.html", data=[{"languages":"English"}, {"languages":"Spanish"}, {"languages":"Finnish"}, {"languages":"German"}, {"languages":"Mandarin"}], translation_text=result)
+
+            case "Spanish":
+                result = spanish(select_end.lower(), translate)
+                return render_template("prototype.html", data=[{"languages":"English"}, {"languages":"Spanish"}, {"languages":"Finnish"}, {"languages":"German"}, {"languages":"Mandarin"}], translation_text=result) 
+
+            case "German":
+                result = german(select_end.lower(), translate)
+                return render_template("prototype.html", data=[{"languages":"English"}, {"languages":"Spanish"}, {"languages":"Finnish"}, {"languages":"German"}, {"languages":"Mandarin"}], translation_text=result) 
+
+            case "Mandarin":
+                result = mandarin(select_end.lower(), translate)
+                return render_template("prototype.html", data=[{"languages":"English"}, {"languages":"Spanish"}, {"languages":"Finnish"}, {"languages":"German"}, {"languages":"Mandarin"}], translation_text=result) 
+
+            case "Finnish":
+                result = finnish(select_end.lower(), translate)
+                return render_template("prototype.html", data=[{"languages":"English"}, {"languages":"Spanish"}, {"languages":"Finnish"}, {"languages":"German"}, {"languages":"Mandarin"}], translation_text=result) 
+
+    return render_template("prototype.html", data=[{"languages":"English"}, {"languages":"Spanish"}, {"languages":"Finnish"}, {"languages":"German"}, {"languages":"Mandarin"}], translation_text="Translation")
 
 if __name__ == "__main__":
     app.run()
